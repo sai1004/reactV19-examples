81aaafb5fa4540801bad701b5fe431278f8206ac
sai1004 <saikiran.bavandla@gmail.com>
2025-03-01 17:49:51 +0530

    commit

--- todo-web-app/src/App.tsx
+++ todo-web-app/src/App.tsx
@@ -1,4 +1,4 @@
-import { useEffect, useState } from "react";
+import { useState } from "react";
 import "./App.css";
 
 interface Todo {
@@ -22,27 +22,27 @@ function App() {
             setTodoList((prev) => {
                 return [...prev, todo];
             });
-            localStorage.setItem("todoList", JSON.stringify(todoList));
+            // localStorage.setItem("todoList", JSON.stringify(todoList));
             setTodo({ id: 0, title: "", completed: false });
         }
     };
 
     const toggleCompleted = (id: number) => {
         setTodoList(todoList.map((todo) => (todo.id == id ? { ...todo, completed: !todo.completed } : todo)));
-        localStorage.setItem("todoList", JSON.stringify(todoList));
+        // localStorage.setItem("todoList", JSON.stringify(todoList));
     };
 
     const deleteTodo = (id: number) => {
         setTodoList(todoList.filter((todo) => todo.id !== id));
-        localStorage.setItem("todoList", JSON.stringify(todoList));
+        // localStorage.setItem("todoList", JSON.stringify(todoList));
     };
 
-    useEffect(() => {
-        console.log("totot", todoList.length);
-        const tododata = JSON.parse(JSON.stringify(localStorage.getItem("todoList")));
-        console.log("tododata", tododata);
-        setTodoList(JSON.parse(tododata));
-    }, []);
+    // useEffect(() => {
+    //     console.log("totot", todoList.length);
+    //     const tododata = JSON.parse(JSON.stringify(localStorage.getItem("todoList")));
+    //     console.log("tododata", tododata);
+    //     setTodoList(JSON.parse(tododata));
+    // }, []);
 
     return (
         <>
